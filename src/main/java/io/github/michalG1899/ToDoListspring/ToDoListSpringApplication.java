571401dca59806fe8d89856680c328f484d73c84
Michal Gebski <gebski.michal@gmail.com>
2020-06-21 20:33:48 +0200

    First commit

---
+++ src/main/java/io/github/michalG1899/ToDoListspring/ToDoListSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.michalG1899.ToDoListspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/michalG1899/ToDoListspring/ToDoListSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.michalG1899.ToDoListspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
