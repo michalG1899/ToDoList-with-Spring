46e2b81339c3cb6dabb3a21c26034195901de7c1
Michal Gebski <gebski.michal@gmail.com>
2020-06-22 21:16:17 +0200

    Changes in index.html

--- src/main/java/io/github/michalG1899/ToDoListspring/todo/TodoServlet.java
+++ src/main/java/io/github/michalG1899/ToDoListspring/todo/TodoServlet.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.criteria.CriteriaBuilder;
@@ -23,7 +24,7 @@ public class TodoServlet extends HttpServlet {
 
     private TodoRepository repository;
 
-    TodoServlet(TodoRepository repository) {
+    public TodoServlet(TodoRepository repository) {
         this.repository = repository;
     }
 
